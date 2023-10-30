dc77d4ef4733cda23d4822fdb8fce41731636951
David Alvarado <alvaradodavid96@gmail.com>
2023-10-30 18:01:38 +0100

    Removes material3 libraries because they are experimental. Adds the IntegerField, MainView, and WordField composables.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ android {
     }
   }
   namespace = "com.dalvarado.fizzbuzz"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.dalvarado.fizzbuzz"
     minSdk = 27
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 
@@ -57,14 +57,14 @@ android {
 
 dependencies {
 
-  implementation("androidx.core:core-ktx:1.9.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-  implementation("androidx.activity:activity-compose:1.7.0")
+  implementation("androidx.core:core-ktx:1.12.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+  implementation("androidx.activity:activity-compose:1.8.0")
   implementation(platform("androidx.compose:compose-bom:2023.03.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
-  implementation("androidx.compose.material3:material3")
+  implementation("androidx.compose.material:material-android:1.5.4")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
