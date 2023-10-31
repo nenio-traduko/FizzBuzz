08493754bf40084650c2a25e7a4c62bb4380d633
David Alvarado <alvaradodavid96@gmail.com>
2023-10-31 09:16:33 +0100

    Increases the version code and name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.dalvarado.fizzbuzz"
     minSdk = 27
     targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
